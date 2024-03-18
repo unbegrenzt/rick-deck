985f46300fe1b2e315f86e5a2fb298ba8b820f60
unbegrenzt <jmoralescenteno@gmail.com>
2024-03-17 19:11:17 -0600

    feat: :sparkles: show toast on enter the view

--- src/components/molecules/ToastContainer.tsx
+++ src/components/molecules/ToastContainer.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-// import Toast, { ToastProps } from 'components/atoms/Toast';
-import useToastStore from 'src/store/useToastStore';
+import Toast from 'components/atoms/Toast';
+import useToastStore from 'store/useToastStore';
 
 const ToastContainer: React.FC = () => {
 
   const toasts = useToastStore((state) => state.toasts);
 
   return (
-    <div>
-      {toasts.length > 0 && toasts.map((toast) => (
-        <p>just a toast {toast.uuid}</p>
+    <div className="fixed flex flex-col items-end top-0 right-0">
+      {toasts.map((toast) => (
+        <Toast key={toast.uuid} {...toast} />
       ))}
     </div>
-
   );
 };
-
-// <div className="fixed flex flex-col items-end top-0 right-0">
-//   {toasts.map((toast) => (
-//     <Toast key={toast.uuid} {...toast} />
-//   ))}
-// </div>
 
 export default ToastContainer;
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,22 +1,31 @@
 import React, { useEffect } from 'react'
 
+import 'react-toastify/dist/ReactToastify.min.css';
+
 import {
   ColumnDef,
 } from '@tanstack/react-table'
-
 import { createLazyFileRoute } from '@tanstack/react-router'
 
 //custom components
 import { fetchFromServer, Character, defaultMetaParameters } from 'services/fetchRicksService'
+
 import RickCard from 'components/molecules/RickCard'
 import RicksTable from 'components/organisms/RicksTable'
 import HeartCheckbox from 'components/atoms/HeartCheckbox'
+import ToastContainer from 'components/molecules/ToastContainer';
+
+import useToastStore from 'store/useToastStore';
+
+import { generateUUIDv4 } from 'utils/stringsUtil';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+
+  const showToast = useToastStore((state) => state.showToast);
 
   const columns = React.useMemo<ColumnDef<Character>[]>(
     () => [
@@ -43,7 +52,13 @@ function Index() {
     []
   )
 
+  useEffect(() => {
+    showToast({ uuid: generateUUIDv4(), message: 'Tienes hasta 3 Mortys en este universo', type: 'info' })
+  }, [])
+
   return (
+    <>
+      <ToastContainer />
       <RicksTable
         {...{
           columns,
@@ -51,5 +66,6 @@ function Index() {
           meta: defaultMetaParameters
         }}
       />
+    </>
   )
 }
